ae7f04043d90ddf20968c74bebad9bcea1c6ff96
Mark Carrier <markcarrier@gmail.com>
2020-12-16 15:12:15 +0300

    Feature image text is black if it starts with *

--- components/post.js
+++ components/post.js
@@ -1,11 +1,17 @@
 export default function Post({ title, slug, html, feature_image }) {
+  let titleImageTextClasses = 'image-text-overlay text-3xl'
+  if (title && title.startsWith('*')) {
+    titleImageTextClasses = titleImageTextClasses + ' text-black'
+    title = title.slice(1)
+  }
+
   return (
     <>
       {feature_image && (
-        <div className="relative text-center text-white">
+        <div className="relative text-center text-white font-bold">
           <img src={feature_image} />
           {title && !title.startsWith('_') && (
-            <div className="image-text-overlay text-3xl">{title}</div>
+            <div className={titleImageTextClasses}>{title}</div>
           )}
         </div>
       )}
