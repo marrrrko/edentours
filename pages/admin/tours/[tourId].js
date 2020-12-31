45db74679d4aae70c0dc37d854ca24bb2cd036ee
Mark Carrier <markcarrier@gmail.com>
2021-01-01 00:25:59 +0300

    Adds mail all link to bookings admin

--- pages/admin/tours/[tourId].js
+++ pages/admin/tours/[tourId].js
@@ -9,39 +9,61 @@ export default function Bookings({ accessGranted, bookings }) {
     return <DefaultErrorPage statusCode={401} />
   }
 
+  const emails = `mailto:?bcc=${bookings.map((b) => b.email).join('&bcc=')}`
+
   return (
-    <div className="px-10 pt-8">
-      <h2>Bookings</h2>
-      <table className="tablet-auto">
-        <thead>
-          <tr>
-            <th>Action</th>
-            <th>Date</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Group name</th>
-            <th>About group</th>
-            <th>Participant Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {bookings.map((t) => {
-            return (
-              <tr key={t.bookingId}>
-                <td className="border px-4 py-2">{t.eventType}</td>
-                <td className="border px-4 py-2">
-                  {format(parseISO(t.eventTime), 'MM/dd/yy - h:mm a zzzz')}
-                </td>
-                <td className="border px-4 py-2">{t.bookerName}</td>
-                <td className="border px-4 py-2">{t.email}</td>
-                <td className="border px-4 py-2">{t.groupName}</td>
-                <td className="border px-4 py-2">{t.groupDetails}</td>
-                <td className="border px-4 py-2">{t.participantCount}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+    <div className="px-10 pt-8 flex flex-col content-center">
+      <div>
+        <h2>Bookings</h2>
+      </div>
+      {bookings.length > 0 && (
+        <div className="block my-8">
+          <a
+            href={emails}
+            target="_blank"
+            className="bg-yellow-200 no-underline text-black text-xs font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-yellow-400 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-red-200"
+          >
+            Email everyone
+          </a>
+        </div>
+      )}
+      <div>
+        <table className="mx-auto">
+          <thead>
+            <tr>
+              <th>Action</th>
+              <th>Date</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Group name</th>
+              <th>About group</th>
+              <th>Participant Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking) => {
+              return (
+                <tr key={booking.bookingId}>
+                  <td className="border px-4 py-2">{booking.eventType}</td>
+                  <td className="border px-4 py-2">
+                    {format(
+                      parseISO(booking.eventTime),
+                      'MM/dd/yy - h:mm a zzzz'
+                    )}
+                  </td>
+                  <td className="border px-4 py-2">{booking.bookerName}</td>
+                  <td className="border px-4 py-2">{booking.email}</td>
+                  <td className="border px-4 py-2">{booking.groupName}</td>
+                  <td className="border px-4 py-2">{booking.groupDetails}</td>
+                  <td className="border px-4 py-2">
+                    {booking.participantCount}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
