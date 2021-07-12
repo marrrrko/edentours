cdff784e248530dac3339a7f99d3df4bf6cf1954
Mark Carrier <markcarrier@gmail.com>
2021-07-12 11:51:57 +0300

    Gives defaults to couch lib to allow build without env file

--- db/tour-guides.js
+++ db/tour-guides.js
@@ -2,7 +2,7 @@ import nano from 'nano'
 
 const COUCH_URL = process.env.COUCH_URL
 const COUCH_DB_PREFIX = process.env.COUCH_DB_PREFIX
-const couch = nano(COUCH_URL)
+const couch = nano(COUCH_URL || "https://admin:password@couch.server/")
 const tourGuidesStore = couch.use(`${COUCH_DB_PREFIX}guides`)
 
 export async function getAllGuides() {
